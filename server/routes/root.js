a220767f6faf21353b8ea0c56709cf18331bb5fa
PrathmTayade <122280972+PrathmTayade@users.noreply.github.com>
2023-05-17 22:51:15 +0530

    testing 6

--- server/routes/root.js
+++ server/routes/root.js
@@ -15,10 +15,71 @@ router.get("/login", (req, res) => {
   res.send({ hello: "world" });
 });
 
-router.post("/login", login);
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // console.log(req.body);
+
+    // Find the user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "User not found, please signup first" });
+    }
+
+    // Compare passwords
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res
+        .status(401)
+        .json({ message: "Please check your email or password" });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign(
+      { userId: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "6h" }
+    );
+
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true, maxAge: 15 * 60 * 1000 })
+      .json({ message: "Login successful", token });
+  } catch (error) {
+    console.error("Error in login API:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 // signup
-router.post("/signup", signUp);
+router.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    console.log(req.body);
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "Signup successful" });
+  } catch (error) {
+    console.error("Error in signup API:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 // logout
 router.get("/logout", logout);
